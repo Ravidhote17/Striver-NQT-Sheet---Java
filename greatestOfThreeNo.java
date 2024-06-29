2eac988bab00210beff2022709bf3fca28b9eb90
Ravidhote17 <ravidhote101@gmail.com>
2024-06-29 17:47:11 +0530

    commit

--- greatestOfThreeNo.java
+++ greatestOfThreeNo.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
-// class greatestOfThreeNo{
-//     public static void main(String[] args){
-//         System.out.print("Enter the value of a: ");
-//         Scanner sc = new Scanner(System.in);
-//         int a = sc.nextInt();
-//         System.out.print("Enter the value of b: ");
-//         int b = sc.nextInt();        
-//         System.out.print("Enter the value of c: ");
-//         int c = sc.nextInt();
+class greatestOfThreeNo{
+    public static void main(String[] args){
+        System.out.print("Enter the value of a: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();        
+        System.out.print("Enter the value of c: ");
+        int c = sc.nextInt();
 
-//         if(a>b && a>c){
-//             System.out.print("a is greater than b and c");
-//         }
-//         else if(b>a && b>c){
-//             System.out.print("b is greater than a and c");
-//         }
-//         else{
-//             System.out.print("c is greater than a and b");
-//         }
-//     }
-// }
+        if(a>b && a>c){
+            System.out.print("a is greater than b and c");
+        }
+        else if(b>a && b>c){
+            System.out.print("b is greater than a and c");
+        }
+        else{
+            System.out.print("c is greater than a and b");
+        }
+    }
+}
 
 // APPROACH 2
 class greatestOfThreeNo{
